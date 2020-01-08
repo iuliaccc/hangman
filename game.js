f774e14944c0af54151cba8c1f083337b2721737
Iulia <iuli.centea@gmail.com>
2020-01-09 01:04:35 +0200

    Final touch

--- game.js
+++ game.js
@@ -4,12 +4,11 @@ window.onload = function () {
     // popup.style.visibility="hidden";
 
 
-    function reset(win = false){
-        // if (!win)
-        //     sessionStorage.setItem('score',0);
+    function reset(keepScore = false){
+        if (!keepScore)
+            sessionStorage.setItem('score',0);
         window.location.reload();
     };
-    document.getElementById("reset").addEventListener("click", reset);
 
     const words=["advice",
         "arrangement",
@@ -44,7 +43,7 @@ window.onload = function () {
         "pixel",
         "zombie",
     ] ;
-    let win = false;
+    let keepScore = false;
     const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
         'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
         't', 'u', 'v', 'w', 'x', 'y', 'z'];
@@ -82,6 +81,8 @@ window.onload = function () {
     // var showClue = document.getElementById("clue");
 
     //let foundedIndexes=[];
+    document.getElementById("reset").addEventListener("click", ()=>reset(keepScore));
+
     let sessionScore=sessionStorage.getItem('score');
     let score;
     if(sessionScore)
@@ -108,7 +109,7 @@ window.onload = function () {
             list = document.createElement("button");
             list.id =alphabet[i];
             list.addEventListener("click",()=>findLetterFromWords(event) );
-            list.innerText= alphabet[i]
+            list.innerText= alphabet[i];
             list.className="letter";
             myButtons.appendChild(letters);
             letters.appendChild(list);
@@ -126,30 +127,29 @@ window.onload = function () {
             }
         }
         if(win===true){
-            // createImg();
-            // // document.getElementById('game-arrangement').style.fontSize="100px";
-            // document.getElementById('game-arrangement').style.color="white";
-            // document.getElementById('game-arrangement').style.textAlign="center";
-            popup=document.getElementById('popup1')
-            popup.style.display="block";
-            document.getElementById('game').innerHTML = 'You Won!!!';
+            document.getElementById('hideImgWin').style.display="block";
+            let all=document.getElementsByClassName('all')[0];
+            let gameArrangement=document.getElementsByClassName('game-arrangement')[0];
+            all.removeChild(gameArrangement);
+            document.getElementById("reset").style.marginTop="60vh";
+
+            // addImg();
+            // document.getElementById("hangmanPic").innerHTML="<img src=\"./images/badass.jpg\"  alt>";
+            // document.getElementById('container').style.fontSize="30px";
+            // document.getElementById('container').style.color="white";
+            // document.getElementById('container').innerHTML = "Yay! You won +1p! Click play again to collect more points";
             score++;
             // score.innerText=score;
             document.getElementById('scoreid').innerText=score;
             console.log(score);
-            sessionStorage.setItem('score',score)
-            win=true
+            sessionStorage.setItem('score',score);
+            keepScore=true;
+            // popup=document.getElementById('popup1');
+            // popup.style.display="block";
+            // console.log(popup)
 
         }
     };
-    // function createImg() {
-    //     var x = document.createElement("IMG");
-    //     x.setAttribute("src", "wn.png");
-    //     x.setAttribute("width", "304");
-    //     x.setAttribute("height", "228");
-    //     x.setAttribute("alt", "The Pulpit Rock");
-    //     document.body.appendChild(x);
-
 
 //caut cuvant random din lista
     let selectedWord = function(){
@@ -158,21 +158,6 @@ window.onload = function () {
 
     };
     selectedWord();
-
-    // let winning = function(){
-    //     let win=false;
-    //     let answer=0;
-    //     for(let i=0;i<word.length;i++){
-    //         if(document.getElementById(i).innerText!=='_'){
-    //             win=true;
-    //             answer++;
-    //             console.log(word[i]);
-    //         }
-    //     }
-    //     if(answer===word.length){
-    //         document.getElementById('game').innerHTML = 'You Won!!!';
-    //     }
-    // };
 
     //daca litera pe care am apasat click exista in word atunci se pune in cuvant
 
@@ -201,12 +186,17 @@ window.onload = function () {
         };
         //console.log("literele cautate se afla pe pozitiile: " +foundedIndexes);
 
+    function addImg() {
+        let img = document.createElement('img');
+        img.src = './images/badass.jpg';
+        document.getElementsByClassName('game-arrangement').innerHtml=img;
+    }
+
     function displayLives(number) {
-        let heartsDiv=document.getElementById('lives').src = './images/hearts.png'
+        //let heartsDiv=document.getElementById('lives').src = './images/hearts.png'
         document.getElementById("lives").innerHTML = 'Lives:  ';
         for (var i = 0; i < number; i++)
             {
-                //let multipyhearts= heartsDiv*i;
                 let a = new Image();
                 a.src = './images/hearts.png';
                 a.width = 25;
@@ -229,11 +219,7 @@ window.onload = function () {
     };
 
 
-    //findLetterFromWords();
-
     // create alphabet ul
-
-
 
     let displayWord = function(){
         let divWord = document.getElementById('chosenWord');
@@ -257,180 +243,28 @@ function  lost() {
         //window.alert("I have spoken. You lost!");
         document.getElementById('container').style.fontSize="30px";
         document.getElementById('container').style.color="white";
-
+        // document.getElementById('hideImgLost').style.display="block";
+        // let all=document.getElementsByClassName('all')[0];
+        // let gameArrangement=document.getElementsByClassName('game-arrangement')[0];
+        // all.removeChild(gameArrangement);
+        // document.getElementById("reset").style.marginTop="60vh";
+        // document.getElementById('lost').style.marginTop="80vh";
         document.getElementById('container').innerHTML = 'You lost.... The answer was: ' + word;
 
 
        }
 }
+    function isLetter(str) {
+        return str.length === 1 && str.match(/[a-z]/i);
+    }
 // winning();
     window.addEventListener("keydown", function (event) {
         if (event.defaultPrevented) {
             return; // Do nothing if the event was already processed
         }
-        if(event.key.charCodeAt()&&'A'.charCodeAt()<=event.key.charCodeAt()&&event.key.charCodeAt()<='z'.charCodeAt()) {
-
-            findLetterIndexes(event.key)
+        if(isLetter(event.key)) {
+            // findLetterIndexes(event.key)
+            document.getElementById(event.key.toLowerCase()).click()
         }
     });
-
-//     var popupEl = document.getElementById('popup');
-// // As a native plugin
-//     var popup = new Popup(popupEl, {
-//         width: 400,
-//         height: 300
-//     });
-//     var open = document.getElementById('open');
-//     open.onclick = function() {
-//         popup.open();
-//     };
-
-
-
-
-
-
-
-//     var btnOpen = select('.js-open');
-//     var btnClose = select('.js-close');
-//     var modal = select('.js-modal');
-//     var modalChildren = modal.children;
-//
-//     function hideModal() {
-//         dynamics.animate(modal, {
-//             opacity: 0,
-//             translateY: 100
-//         }, {
-//             type: dynamics.spring,
-//             frequency: 50,
-//             friction: 600,
-//             duration: 1500
-//         });
-//     }
-//
-//     function showModal() {
-//         // Define initial properties
-//         dynamics.css(modal, {
-//             opacity: 0,
-//             scale: .5
-//         });
-//
-//         // Animate to final properties
-//         dynamics.animate(modal, {
-//             opacity: 1,
-//             scale: 1
-//         }, {
-//             type: dynamics.spring,
-//             frequency: 300,
-//             friction: 400,
-//             duration: 1000
-//         });
-//     }
-//
-//     function showBtn() {
-//         dynamics.css(btnOpen, {
-//             opacity: 0
-//         });
-//
-//         dynamics.animate(btnOpen, {
-//             opacity: 1
-//         }, {
-//             type: dynamics.spring,
-//             frequency: 300,
-//             friction: 400,
-//             duration: 800
-//         });
-//     }
-//
-//     function showModalChildren() {
-//         // Animate each child individually
-//         for(var i=0; i<modalChildren.length; i++) {
-//             var item = modalChildren[i];
-//
-//             // Define initial properties
-//             dynamics.css(item, {
-//                 opacity: 0,
-//                 translateY: 30
-//             });
-//
-//             // Animate to final properties
-//             dynamics.animate(item, {
-//                 opacity: 1,
-//                 translateY: 0
-//             }, {
-//                 type: dynamics.spring,
-//                 frequency: 300,
-//                 friction: 400,
-//                 duration: 1000,
-//                 delay: 100 + i * 40
-//             });
-//         }
-//     }
-//
-//     function toggleClasses() {
-//         toggleClass(btnOpen, 'is-active');
-//         toggleClass(modal, 'is-active');
-//     }
-//
-// // Open nav when clicking sandwich button
-//     btnOpen.addEventListener('click', function(e) {
-//         toggleClasses();
-//         showModal();
-//         showModalChildren();
-//     });
-//
-// // Open nav when clicking sandwich button
-//     btnClose.addEventListener('click', function(e) {
-//         hideModal();
-//         dynamics.setTimeout(toggleClasses, 500);
-//         dynamics.setTimeout(showBtn, 500);
-//     });
-
 };
-
-
-    //
-    //
-    // let mask = function () {
-    //
-    //     let wordHolder = document.getElementById()
-    //     let correct = document.createElement('ul');
-    //     for (let i = 0; i < letterSelected.length; i++) {
-    //         picked = document.createElement('li');
-    //         picked.setAttribute('class', 'picked');
-    //         if (letterSelected[i] === "-") {
-    //             picked.innerHTML = "-";
-    //             space = 1;
-    //         } else {
-    //             picked.innerHTML = "_";
-    //         }
-    //         pickedList.push(picked);
-    //         wordHolder.appendChild(correct);
-    //         correct.appendChild(picked);
-    //         //styling ul
-    //         correct.style.margin = "0";
-    //         correct.style.display = "block";
-    //         correct.style.padding = "0";
-    //         //li
-    //         picked.style.position = "relative";
-    //         picked.style.listStyle = "none";
-    //         picked.style.margin = "0";
-    //         picked.style.display = "inline-block";
-    //         picked.style.padding = "0 10px";
-    //         picked.style.fontSize = "1.6em";
-    //     }
-    // };
-
-
-    // let display = function () { //print outs for end game
-    //     showLives.innerHTML = "You have " + lives + " lives";
-    //     if (lives < 1) {
-    //         showLives.innerHTML = "Game Over! The letterSelected was "
-    //             + letterSelected + ".";
-    //     }
-    //     for (var i = 0; i < pickedList.length; i++) {
-    //         if (total + space === pickedList.length) {
-    //             showLives.innerHTML = "You Win!";
-    //         }
-    //     }
-    // }
